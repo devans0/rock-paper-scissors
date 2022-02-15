4e126fb98956e2182da16b601cec71533899db0f
Dominic Evans <evans.dominic0@gmail.com>
2022-02-14 19:40:49 -0600

    Added index.html and core logic for rock paper scissors game

---
+++ script/logic.js
@@ -0,0 +1,114 @@
+// Logic for Rock Paper Scissors game created as part of The Odin Project
+//
+// Game outline:
+//   - Ask user for a selection (Rock, Paper or Scissors)
+//   - Determine what the game will choose to play
+//   - Compare results of user and game selections
+//   - Report to the user whether they have won or lost
+
+// Function to create a random choice for the computer to play
+// Randomize a value between 1 and 3 inclusive and then assign a selection to the computer
+// based on that random number.
+function computerPlay(){
+  let randomSelect = Math.floor(Math.random() * 3) + 1;
+  let result;
+  switch (randomSelect){
+    case 1:
+      return result = "Rock";
+      break;
+    case 2:
+      return result  = "Paper";
+      break;
+    case 3:
+      return result = "Scissors";
+      break;
+  }
+}
+
+// Function that compares the value of the computer's selection against that of the user
+// then outputs the value of the result to the console.
+function playRound(playerSelection, computerSelection){
+  const playerSelect = playerSelection.toLowerCase();
+  const computerSelect = computerSelection.toLowerCase();
+
+  let result;
+
+  switch (playerSelect){
+    case 'rock':
+      if (computerSelect == 'rock'){
+        result = 'tie';
+        return result;
+      } else if (computerSelect == 'paper'){
+        result = 'lose';
+        return result;
+      } else if (computerSelect == 'scissors'){
+        result = 'win';
+        return result;
+      }
+
+    case 'paper':
+      if (computerSelect == 'rock'){
+        result = 'win';
+        return result;
+      } else if (computerSelect == 'paper'){
+        result = 'tie';
+        return result;
+      } else if (computerSelect == 'scissors'){
+        result = 'lose';
+        return result;
+      }
+
+    case 'scissors':
+      if (computerSelect == 'rock'){
+        result = 'lose';
+        return result;
+      } else if (computerSelect == 'paper'){
+        result = 'win';
+        return result;
+      } else if (computerSelect == 'scissors'){
+        result = 'tie';
+        return result;
+      }
+
+    default:
+      result = 'error';
+      return result;
+  }
+}
+
+// Function to run the game for five rounds, displaying the score and prompting the user for
+// each step the game is run
+function game(){
+  let playerChoice;
+  let computerChoice;
+  let roundResult;
+  let computerScore = 0;
+  let playerScore = 0;
+
+  for(let i = 0; i < 5; i++){
+    playerChoice = prompt("Rock, Paper or Scissors?");
+    computerChoice = computerPlay();
+
+    roundResult = playRound(playerChoice, computerChoice);
+
+    if (roundResult == 'tie'){
+      console.log(`This round is a tie! Score is You: ${playerScore}; Computer: ${computerScore}`);
+    } else if (roundResult == 'win'){
+      playerScore += 1;
+      console.log(`You win the round! Score is You: ${playerScore}; Computer: ${computerScore}`);
+    } else if (roundResult == 'lose'){
+      computerScore += 1;
+      console.log(`You lose the round! Score is You: ${playerScore}; Computer: ${computerScore}`);
+    }
+  }
+
+  if (playerScore > computerScore){
+    console.log("Congratulations, you won the game!");
+  } else if (computerScore > playerScore){
+    console.log("You lost the game! Better luck next time!");
+  } else {
+    console.log("The game is a tie!");
+  }
+}
+
+game();
